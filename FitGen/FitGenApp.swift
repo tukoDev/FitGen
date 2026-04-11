3bb6dcede777b88a7a5246372b4e6b98e0512e7e
Turker <turkerrgenc8@gmail.com>
2026-04-11 12:43:14 +0200

    Initial Commit

---
+++ FitGen/FitGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitGenApp.swift
+//  FitGen
+//
+//  Created by Turker on 11.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct FitGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
